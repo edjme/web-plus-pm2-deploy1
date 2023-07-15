0fb7ffdd7af692dd0a9b1e0d1461bf2aaa736ded
Stanislav <stasikparmut@yandex.ru>
2023-07-16 00:42:07 +0300

    first try

---
+++ README.md
@@ -0,0 +1,7 @@
+# Деплой приложения на сервер с использованием pm2
+
+Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+### IP адрес: 158.160.74.60
+### Frontend: https://pm2.webplus.hloud.tw1.ru
+### Backend: https://api.pm2.webplus.hloud.tw1.ru
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = "origin/master",
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-frontend",
+      script: "./build/index.html",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/frontend`,
+      "post-deploy": "cd frontend && npm i && npm run build",
+    },
+  },
+};
